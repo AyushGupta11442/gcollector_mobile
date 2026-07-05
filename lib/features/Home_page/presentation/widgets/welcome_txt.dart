11feb26631b0a42202eb5c73ae6500755ec38689
AyushGupta11442 <ayushgupta11442@gmail.com>
2026-07-05 19:16:14 +0530

    user card made

--- lib/features/Home_page/presentation/widgets/welcome_txt.dart
+++ lib/features/Home_page/presentation/widgets/welcome_txt.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gcollector_mobile/core/theme/app_theme.dart' show AppTheme;
 
 class HomeViewPersonalInfo extends StatelessWidget {
   final String PersonName;
@@ -9,44 +10,120 @@ class HomeViewPersonalInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      
       decoration: BoxDecoration(
         color: Colors.white,
       ),
       padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Welcome back!",
+                      style: const TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      PersonName,
+                      style: const TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.indigo,
+                  ),
+                  child: const Icon(Icons.person, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          YourEarningContainer(),
+        ],
+      ),
+    );
+  }
+}
+
+class YourEarningContainer extends StatelessWidget {
+  const YourEarningContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: AppTheme.primaryGradient,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                "Welcome back!",
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+              Row(
+                children: [
+                  Icon(Icons.wallet, color: Colors.white),
+                  const Text(
+                    "Your Earnings",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                ],
               ),
-              Text(
-                PersonName,
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
+              const Text(
+                    "View Details",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
             ],
           ),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.indigo,
+          const SizedBox(height: 8.0),
+          const Text(
+            "\$1,234.56",
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            child: const Icon(Icons.person, color: Colors.white),
           ),
+          Row(
+            children: [
+              const Icon(Icons.arrow_upward_outlined, color: Colors.white),
+              Text(
+                "+rupee 340",
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
